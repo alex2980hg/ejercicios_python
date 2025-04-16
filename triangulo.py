269c09fc16a873facdb26fa9245bade7108a79ba
alexander <alexander.kristobal2590@gmail.com>
2025-04-16 00:50:11 -0500

    Triangulo actualizacion

--- triangulo.py
+++ triangulo.py
@@ -6,5 +6,10 @@ ladoc = int(input("\nINGRESA EL LADO C: "))
 if ladoa == ladob and ladoc == ladoa: 
     print("ES UN TRIANGULO EQUILATERO")
 
-else: 
-    print ("NO ES UN TRIANGULO EQUILATERO")
+elif (ladoa == ladob and ladoa != ladoc) or (ladob == ladoc and ladob != ladoa) or (ladoa == ladoc and ladoc != ladob):
+
+    print ("ES UN TRIANGULO ISOSCELES")
+
+else:
+
+    print ("ES UN TRIANGULO ESCALENO")
