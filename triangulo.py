2bc90dceb2d9cba74790d979e8e93b56f6ecba89
alexander <alexander.kristobal2590@gmail.com>
2025-04-16 00:18:51 -0500

    NUEVO ARCHIVO PYTHON

---
+++ triangulo.py
@@ -0,0 +1,7 @@
+
+ladoa = int(input("INGRESA EL LADO A: "))
+ladob = int(input("\nINGRESA EL LADO B: "))
+ladoc = int(input("\nINGRESA EL LADO C: "))
+
+if ladoa == ladob and ladoc == ladoa: 
+    print("ES UN TRIANGULO EQUILATERO")
